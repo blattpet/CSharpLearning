b5894b9bc3c36c66e94b23145176af74e429db12
Peter Blattmann <blattmann.peter@bluewin.ch>
2023-04-23 23:17:32 +0200

    Added chapter 9 exercise 5
    TODO tasks one and two

---
+++ Basics/Chapter09/Exercise5/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskOne();
+            //TaskTwo();
+        }
+
+        public static void TaskOne()
+        {
+
+        }
+
+        public static void TaskTwo()
+        {
+
+        }
+    }
+}
