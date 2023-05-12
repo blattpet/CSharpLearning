6db1a8d7a8394e118d1d85469fee5363d819ed40
Peter Blattmann <blattmann.peter@bluewin.ch>
2023-05-12 15:57:30 +0200

    Intermediate added chapter 2 exercise 2
    Post Application, working with properties

---
+++ Intermediate/Chapter02/Exercise2/Post.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise2
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Time { get; set; }
+
+        public int Votes { get; private set; }
+
+        public void UpVoting()
+        {
+            Votes++;
+        }
+
+        public void DownVoting()
+        {
+            Votes--;
+        }
+    }
+}
---
+++ Intermediate/Chapter02/Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post = new Post();
+            post.Title = "Test";
+            post.Description = "This is a test with properties.";
+            post.Time = DateTime.Now;
+
+            post.UpVoting();    // 1
+            post.UpVoting();    // 2
+            post.UpVoting();    // 3
+            post.UpVoting();    // 4
+            post.DownVoting();  // 3
+
+            Console.WriteLine("Title: {0}", post.Title);
+            Console.WriteLine("Description: {0}", post.Description);
+            Console.WriteLine("Created: {0}", post.Time.ToString("yyyy-MM-dd HH:mm"));
+            Console.WriteLine("Votes: {0}", post.Votes);
+        }
+    }
+}
