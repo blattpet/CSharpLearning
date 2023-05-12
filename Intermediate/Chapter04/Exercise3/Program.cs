2e592729d1fa022e7793f452379576febd77cf00
Peter Blattmann <blattmann.peter@bluewin.ch>
2023-05-12 15:18:57 +0200

    Intermediate added chapter 4 exercise 3
    Stack Application

---
+++ Intermediate/Chapter04/Exercise3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ Intermediate/Chapter04/Exercise3/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+namespace Exercise3
+{
+    public class Stack
+    {
+        private readonly ArrayList _stack;
+
+        public Stack() 
+        { 
+            _stack = new ArrayList();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("You can't push an object that is null on the stack!");
+            }
+
+            _stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_stack.Count == 0)
+            {
+                throw new InvalidOperationException("The stack was empty!");
+            } 
+                
+            object element = _stack[_stack.Count - 1];
+            _stack.RemoveAt(_stack.Count - 1);
+            return element;
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+    }
+}
